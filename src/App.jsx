893d7f5cb4fd688d84f357f138f8365be521ab79
Dinara <dinara.kspanovr@gmail.com>
2025-04-06 23:18:59 +0600

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Routes, Route } from 'react-router';
+import Layout from "../src/Layout/Layout/Layout.jsx";
+import MentorPage from "./pages/mentorpage/MentorPage.jsx";
+import ProjectsPage from "./pages/ProjectPage/ProjectsPage.jsx";
+import СoursesPage from "./pages/СoursesPage/СoursesPage.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
 
-const App = () => {
+function App() {
     return (
         <div>
-            <div> </div>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<HomePage />} />
+                    <Route path="mentors" element={<MentorPage />} />
+                    <Route path="projects" element={<ProjectsPage />} />
+                    <Route path="courses" element={<СoursesPage />} />
+                </Route>
+            </Routes>
         </div>
     );
-};
+}
 
 export default App;
