852227862a2605bc90dcd9e624462a414353d7be
BAKYTq1 <bsainaev@gmail.com>
2025-04-08 12:54:04 +0600

    mentors and projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import Mentor from './Mentor/Mentor';
+import Project from './Projects/Project';
 const App = () => {
     return (
         <div>
-            <div> </div>
+            <div><Mentor/>
+            <Project/>
+            </div>
         </div>
     );
 };
